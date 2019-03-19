9c439d1a8f840c4d8af7e569d5722a84d2d0548e
Brett Best <brettbest@live.com.au>
2019-03-20 04:29:01 +1100

    Fix to build on RPi.

--- Package.swift
+++ Package.swift
@@ -2,13 +2,30 @@
 
 import PackageDescription
 
+#if os(macOS)
+
 let package = Package(
   name: "Morse_GUI",
   dependencies: [
     .package(url: "https://github.com/uraimo/SwiftyGPIO.git", .branch("master")),
-    .package(url: "https://github.com/rhx/SwiftGtk.git", .branch("master")),
+    .package(url: "https://github.com/Brett-Best/SwiftGtk.git", .branch("master")),
   ],
   targets: [
     .target(name: "Morse_GUI", dependencies: ["SwiftyGPIO", "Gtk"]),
   ]
 )
+
+#else
+
+let package = Package(
+  name: "Morse_GUI",
+  dependencies: [
+    .package(url: "https://github.com/uraimo/SwiftyGPIO.git", .branch("master")),
+    .package(url: "https://github.com/Brett-Best/SwiftGtk.git", .branch("bb-pkg-dev")),
+  ],
+  targets: [
+    .target(name: "Morse_GUI", dependencies: ["SwiftyGPIO", "Gtk"]),
+  ]
+)
+
+#endif
